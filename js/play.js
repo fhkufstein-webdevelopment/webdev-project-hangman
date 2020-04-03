759582343e76ea7a55e7838f8eb4063e5d947911
alica waibl <alicawaibl@dellneu.home>
2020-04-03 13:45:12 +0200

    js: zeit(), gamestarten()

--- js/play.js
+++ js/play.js
@@ -1,13 +1,46 @@
 var words = ['Grundstücksverkehrsgenehmigungszuständigkeitsübertragungsverordnung','Rindfleischetikettierungsüberwachungsaufgabenübertragungsgesetz','Telekommunikationsüberwachungsverordnung'];
 
+
 function zufallszahl(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-words[zufallszahl(0 , words.length - 1)];
 
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+if(document.getElementById("startbutton").innerHTML("onclick")){
+    gamestarten();
+}
 
-var points ;
 
+function gamestarten(){
+    words[zufallszahl(0 , words.length - 1)];
+    document.getElementById(wort).innerHTML = print(words);
+    doument.getElementById("countdown").innerHTML = zeit();
+}
 
+function zeit(){
+    var minuten = 1;
+    var sekunden = 59;
+    minuten--;
+    sekunden--;
+
+    if(minuten == 0)document.getElementById("countdown").innerHTML = "00:"+sekunden;
+    else if(sekunden<10){
+        document.getElementById("countdown").innerHTML = "00:0"+sekunden;
+        document.getElementById("countdown").style.color = red;
+    }
+}
+
+function play(){
+    var buchstabe;
+    if(buchstabe = indexOf.words){
+        document.getElementById("rightsound").play();
+    }else{
+        document.getElementById("failsound").play();
+    }
+}
+
+//wie handhaben mit buchstaben?
+
+//Balken richtig/falsch
+
+//einbinden in html
