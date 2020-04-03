dc94357b3c6fafcecba4d0fc9b49fe81566d3836
alica waibl <alicawaibl@dellneu.home>
2020-04-03 10:35:07 +0200

    kleine Veränderung js

--- js/play.js
+++ js/play.js
@@ -5,3 +5,9 @@ function zufallszahl(min, max) {
 }
 
 words[zufallszahl(0 , words.length - 1)];
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var points ;
+
+
