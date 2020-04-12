79f75016e67090f7ca89d91775cc8542c33167f0
Katharina Mader <katharinamader@katharinas-air.home>
2020-04-12 08:30:32 +0200

    Loginseite, Footer und Startseite- in HTML

--- includes/views/parts/footer.php
+++ includes/views/parts/footer.php
@@ -4,7 +4,11 @@
 <head> </head>
 
 <body>
+
+<div class="glyphicon-text-background">
     <p class="text-center">&copy; HANGMAN <br>Alica - Chanelle - Julia - Katharina</p>
+
+</div>
 </body>
 
 </html>
