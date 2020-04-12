c0363e69d84a608ca67f9ad34a442e8a203cfea6
Katharina Mader <katharinamader@katharinas-air.home>
2020-04-12 22:04:13 +0200

    Header.php

--- includes/views/parts/header.php
+++ includes/views/parts/header.php
@@ -15,9 +15,9 @@
 
 
     <?php if($this->current == "login"): ?>
-    <link rel="stylesheet" href="css/body.css">
-    <link rel="stylesheet" href="css/login-microsite.css">
 
+    <link rel="stylesheet" href="css/login-microsite.css">
+    <link rel="stylesheet" href="css/body.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
 
 
@@ -125,7 +125,7 @@
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+<script src="js/core.js"></script>
         <script src="js/headerFade.js"></script>
 
 
@@ -145,6 +145,9 @@
         <script src="js/headerFade.js"></script>
 
 
+    <?php elseif ($this->current == "register"): ?>
+
+        <script src="js/register.js"></script>
 
     <?php endif; ?>
 
